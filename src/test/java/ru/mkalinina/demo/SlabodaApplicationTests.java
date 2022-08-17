b5915517413eb937c54ee4509723dd2956ca6d32
2MD <moty-92@mail.ru>
2022-08-17 19:02:14 +0300

    Create new project
    Create

---
+++ src/test/java/ru/mkalinina/demo/SlabodaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mkalinina.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlabodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
